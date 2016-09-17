4d12319c9400c890d74f17aa7d62736281a3c73c
Lorenzo Ferrante <lorenzoferrante96@gmail.com>
2016-09-17 16:16:57 +0200

    Updated file with path.join()

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const request = require('request')
 const fs = require('fs')
+const path = require('path')
 let cheerio = require('cheerio')
+
 let trendURL = 'http://hawttrends.appspot.com/api/terms/'
+const file_data = path.join(__dirname, 'data.json') 
 
 // COUNTRY CODE:
 // Italy    27
@@ -9,11 +12,11 @@ let trendURL = 'http://hawttrends.appspot.com/api/terms/'
 
 request(trendURL, (err, res, body) => {
     if (!err && res.statusCode == 200) {
-        fs.writeFile('res.json', body, (err) => {
+        fs.writeFile(file_data, body, (err) => {
             if (err) throw err
             console.log('File saved!')
             // Una volta scritto il file, posso leggerlo come JSON
-            var content = fs.readFileSync('res.json')
+            var content = fs.readFileSync(file_data)
             var data = JSON.parse(content)
 
             console.log(data['27'])
